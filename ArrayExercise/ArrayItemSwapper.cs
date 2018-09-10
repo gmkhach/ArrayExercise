1d0af01347f4579ae1a5340e86f3756d3bfc316d
Gabriel Khachatryan <gmkhach@gmail.com>
2018-09-10 07:52:48 -0400

    first version of the swap() method is implemented

---
+++ ArrayExercise/ArrayItemSwapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExercise
+{
+    class ArrayItemSwapper
+    {
+        public ArrayItemSwapper(int[] array)
+        {
+            this.array = array;
+        }
+
+        private int[] array;
+
+        public void swapArrayItems(int x, int y)
+        {
+            int valueX = array[x];
+            int valueY = array[y];
+            array[x] = valueY;
+            array[y] = valueX;
+        }
+    }
+}
--- ArrayExercise/Program.cs
+++ ArrayExercise/Program.cs
@@ -10,6 +10,13 @@ namespace ArrayExercise
     {
         static void Main(string[] args)
         {
+
+            int[] originalArray = { 1, 2, 3, 4, 5 };
+            Console.WriteLine($"this is the original array: {originalArray[0]}, {originalArray[1]}, {originalArray[2]}, {originalArray[3]}, {originalArray[4]}");
+            ArrayItemSwapper mySwapper = new ArrayItemSwapper(originalArray);
+            mySwapper.swapArrayItems(0, 4);
+            Console.WriteLine($"this is the array after swap method is called: {originalArray[0]}, {originalArray[1]}, {originalArray[2]}, {originalArray[3]}, {originalArray[4]}");
         }
+
     }
 }
